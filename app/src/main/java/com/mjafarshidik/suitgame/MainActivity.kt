66543e40b2704b9e03260606f4302dc2a4d3fd00
mjafarshidik <mjafarshidik86@gmail.com>
2022-03-03 12:00:49 +0700

    Implement main activity

--- app/src/main/java/com/mjafarshidik/suitgame/MainActivity.kt
+++ app/src/main/java/com/mjafarshidik/suitgame/MainActivity.kt
@@ -1,11 +1,26 @@
 package com.mjafarshidik.suitgame
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.mjafarshidik.suitgame.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var mainBinding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        mainBinding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(mainBinding.root)
+        btnStart()
+    }
+
+    private fun btnStart() {
+        mainBinding.apply {
+            btnStart.setOnClickListener {
+                val intent = Intent(this@MainActivity, SuitActivity::class.java)
+                startActivity(intent)
+            }
+        }
     }
 }
---
+++ app/src/main/java/com/mjafarshidik/suitgame/SuitActivity.kt
@@ -0,0 +1,11 @@
+package com.mjafarshidik.suitgame
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SuitActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_suit)
+    }
+}
