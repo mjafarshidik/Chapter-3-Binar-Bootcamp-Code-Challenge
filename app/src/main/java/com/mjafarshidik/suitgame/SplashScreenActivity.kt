59cd1dc21101d73837e70a291a05ed1dea4e8462
mjafarshidik <mjafarshidik86@gmail.com>
2022-03-03 10:28:26 +0700

    Implement Splash Screen Activity

--- app/src/main/java/com/mjafarshidik/suitgame/SplashScreenActivity.kt
+++ app/src/main/java/com/mjafarshidik/suitgame/SplashScreenActivity.kt
@@ -1,13 +1,38 @@
 package com.mjafarshidik.suitgame
 
 import android.annotation.SuppressLint
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.mjafarshidik.suitgame.databinding.ActivitySplashScreenBinding
+import kotlinx.coroutines.*
 
 @SuppressLint("CustomSplashScreen")
 class SplashScreenActivity : AppCompatActivity() {
+    private lateinit var splashScreenBinding: ActivitySplashScreenBinding
+    private val time = 2000L
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_splash_screen)
+        splashScreenBinding = ActivitySplashScreenBinding.inflate(layoutInflater)
+        setContentView(splashScreenBinding.root)
+        withCoroutine(time)
+    }
+
+    private fun withCoroutine(time: Long) {
+        val mScope = CoroutineScope(Dispatchers.IO)
+        mScope.launch {
+            delay(time)
+            withContext(Dispatchers.Main) {
+                launchPostSplashActivity()
+                mScope.cancel(null)
+            }
+        }
+    }
+
+    private fun launchPostSplashActivity() {
+        val intent = Intent(this, MainActivity::class.java)
+        startActivity(intent)
+        finish()
     }
 }
